d6ee436af7eb1502ae253ac98dcac138d65f627e
Zardex1337 <82034172+Zardex1337@users.noreply.github.com>
2022-02-18 14:09:53 +0600

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def drawing():
 
 def follow():
   drawing()
-  os.system("cls & title Zardex Spotify Follow Bot [.Zardex#1337]")
+  os.system("cls & title Zardex Spotify Follow Bot [Zardex?#1337]")
   username = input("[\x1B[32m>\x1B[0m] Username to Follow: ")
   with open("tokens.txt") as file:
       while (token := file.readline().rstrip()):
@@ -23,4 +23,4 @@ def follow():
           r = requests.put(f"https://api.spotify.com/v1/me/following?type=user&ids={username}", headers=headers)
           print(r.status_code)
 follow()
-#Zardex#1337 on discord
+#Zardex?#1337 on discord
